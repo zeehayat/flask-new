546c599d4746891549c57021b3376c926374ad50
zeehayat <zeenux@gmail.com>
2024-11-28 15:56:01 +0500

    TT

--- flaskProject/models.py
+++ flaskProject/models.py
@@ -17,6 +17,7 @@ class User(UserMixin, db.Model):
     password_hash=db.Column(db.String(128), nullable=False)
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
