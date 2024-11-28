598d90088f82c503a2963867a14be063a9c40334
zeehayat <zeenux@gmail.com>
2024-11-28 14:38:55 +0500

    TP

--- flaskProject/form.py
+++ flaskProject/form.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username does not exist')
--- flaskProject/routes.py
+++ flaskProject/routes.py
@@ -1,22 +1,68 @@
-from flask import render_template, flash, redirect
+from datetime import datetime
 
-from app import app
-from form import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 
+from app import app, db
+from form import LoginForm, RegistrationForm
+from models import User
+import werkzeug.urls
 
+@login_required
 @app.route('/')
-def hello_world():  # put application's code here
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
-
-@app.route('/test')
-def test():
+@app.route('/index')
+def index():
     return 'Hello Test World!'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form=LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc !='':
+            next_page = url_for('login')
+        return redirect(url_for('login'))
+
     return render_template('login.html', title='Home', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user= User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts=[
+        {'author': user , 'body': 'Test Post # 1'},
+        {'author': user, 'body': 'Test Post # 2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
